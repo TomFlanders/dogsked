383442ade3a357165bfbfb2508fe68fbbe410009
Tom Flanders <tom@tomflanders.com>
2016-07-12 13:12:33 -0700

    Added test route

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/test', function (req, res) {
+  res.type('text/plain');
+  res.send('This is a test');
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
