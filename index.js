d25c7a47ae1acb666c4440eb7dc273bc75491569
Tom Flanders <tom@tomflanders.com>
2016-07-13 15:11:21 -0700

    Removed legacy code

--- index.js
+++ index.js
@@ -7,21 +7,17 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.get('/test', function (req, res) {
+app.get('/', function (req, res) {
   res.type('text/plain');
   res.send('This is a test');
 })
 
 app.get('/listEvents', function (req, res) {
    fs.readFile(__dirname + "/public/bikesked.json", 'utf8', function (err, data) {
-       console.log( data );
+       console.log( "Hit" );
        res.end( data );
    });
 })
