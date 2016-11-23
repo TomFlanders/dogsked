da346b9f377a5d104c675c9530455a3d7fb0b9dc
Tom Flanders <tom@tomflanders.com>
2016-11-22 19:30:54 -0800

    schedule updated

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var git = require('gulp-git');
+
+
+gulp.task('gitter', function () {
+    gulp.src('.')
+      .pipe(git.add({args: '*'}))
+      .pipe(git.commit('schedule updated'))
+      ;
+});
